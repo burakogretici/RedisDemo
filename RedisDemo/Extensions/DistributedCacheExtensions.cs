50d59e070ba373a69914e7728758a60270cbc1a1
Burak Öğretici <burakogreticii@gmail.com>
2022-03-30 15:30:08 +0300

    Add project files.

---
+++ RedisDemo/Extensions/DistributedCacheExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.Json;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace RedisDemo.Extensions
+{
+    public static class DistributedCacheExtensions
+    {
+        public static async Task SetRecordAsync<T>(this IDistributedCache cache, string recordId, T data,
+            TimeSpan? absoluteExpireTime = null, TimeSpan? unusedExperieTime = null)
+        {
+            var options = new DistributedCacheEntryOptions();
+
+            options.AbsoluteExpirationRelativeToNow = absoluteExpireTime ?? TimeSpan.FromSeconds(60);
+            options.SlidingExpiration = unusedExperieTime;
+
+            var jsonData = JsonSerializer.Serialize(data);
+            await cache.SetStringAsync(recordId, jsonData, options);
+        }
+
+        public static async Task<T?> GetRecordAsync<T>(this IDistributedCache cache, string recordId)
+        {
+            var jsonData = await cache.GetStringAsync(recordId);
+            if (jsonData is null)
+            {
+                return default(T);
+            }
+
+            return JsonSerializer.Deserialize<T>(jsonData);
+        }
+
+    }
+}
